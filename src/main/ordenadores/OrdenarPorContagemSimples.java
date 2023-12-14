f48c52eb8e666e88d2e844bf0a9101413c2e424e
teixeiralucs <89080606+teixeiralucs@users.noreply.github.com>
2023-12-14 19:52:35 +0000

    Contagem Simples e Particiona

--- src/main/ordenadores/OrdenarPorContagemSimples.java
+++ src/main/ordenadores/OrdenarPorContagemSimples.java
@@ -4,7 +4,19 @@ public class OrdenarPorContagemSimples implements Ordenador{
 
     @Override
     public int[] ordene(int[] array) {
-
+        int[] count = new int[array.length];
+        for (int i = 0; i < count.length; i++) {
+            count[i] = 0;
+          }
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+        }
+        int i = 0;
+        for (int j = 0; j < count.length; j++) {
+            for (int k = 0; k < count[j]; k++) {
+                array[i++] = j;
+    }
+  }
         return array;
     }
     
--- src/main/ordenadores/OrdenarQuick.java
+++ src/main/ordenadores/OrdenarQuick.java
@@ -4,11 +4,25 @@ public class OrdenarQuick implements Ordenador{
     @Override
     public int[] ordene(int[] array) {
 
+
         return array;
     }
-
-    public int quickParticio(int[] array, int inicio, int fim){
-        return 0;
-    }
     
+
+    public int quickParticio(int[] array, int inicio, int Fim){
+        int pivot = array[Fim];
+        int i = inicio - 1;
+        for (int j = inicio; j < Fim; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int reserva = array[i+1];
+                array[i+1] = array[Fim];
+                array[Fim] = reserva;
+            }
+        }
+        int reserva = array[i +1];
+        array[i +1] = array[Fim];
+        array[Fim] = reserva;
+        return i;
+    }
 }
